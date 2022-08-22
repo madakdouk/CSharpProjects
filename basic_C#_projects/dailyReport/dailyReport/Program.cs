631e00b22eab5a0e9d219236438884f49a498ef2
Maher Dakdouk <mdakdouk2015@outlook.com>
2022-08-22 18:33:53 -0400

    creating daily report console applciation

---
+++ basic_C#_projects/dailyReport/dailyReport/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace dailyReport
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //printing intro to console
+            Console.WriteLine("The Tech Academy\nStudent Daily Report\n");
+
+            //asking q1
+            Console.WriteLine("What is your Name?");
+            //creating var to store answer to q1
+            string yourName = Console.ReadLine();
+
+            //asking q2
+            Console.WriteLine("\nWhat course are you on?");
+            //creating var to store answer to q2
+            string yourCourse = Console.ReadLine();
+
+            //asking q3
+            Console.WriteLine("\nWhat page number?");
+            //creating var to store answer to q3, converting from string to short in one line
+            short yourPage = Convert.ToInt16(Console.ReadLine());
+
+            //asking q4
+            Console.WriteLine("\nDo you need help with anything? Please answer “true” or “false.”?");
+            //creating var to store answer to q4, converting from string to bool in one line
+            bool needHelp = Convert.ToBoolean(Console.ReadLine());
+
+            //asking q5
+            Console.WriteLine("\nWere there ay positive experiences you'd like to share? Please give specifics.");
+            //creating var to store answer to q5
+            string dailyPositive = Console.ReadLine();
+
+            //asking q6
+            Console.WriteLine("\nIs there any other feedback you’d like to provide? Please be specific.");
+            //creating var to store answer to q6
+            string yourFeedback = Console.ReadLine();
+
+            //asking q7
+            Console.WriteLine("\nHow many hours did you study today?");
+            //creating var to store answer to q7, converting to byte in one line
+            byte yourHours = Convert.ToByte(Console.ReadLine());
+
+            //Leaving console open until user decides to exit
+            Console.ReadLine();
+            
+        }
+    }
+}
