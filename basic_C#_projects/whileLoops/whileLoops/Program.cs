4463106a30dbaae9dade4a458730cc888c346d51
Maher Dakdouk <mdakdouk2015@outlook.com>
2022-08-25 16:13:56 -0400

    demonstrating while loop and do while loop

---
+++ basic_C#_projects/whileLoops/whileLoops/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace whileLoops
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //displaying intro
+            Console.WriteLine("Let's count to five!\n");
+
+            //declaring counter variable
+            byte i = 1;
+
+            //loop will be executed while counter is less than 6
+            while (i <= 5)
+            {
+                //print the counter to the console and break to next line
+                Console.WriteLine(i+ "\n");
+                //update counter
+                i++;
+            }
+
+            //diplaying next message
+            Console.WriteLine("Now, let's continue to count until 10.\n");
+
+            //starting do while loop
+            do
+            {
+                //this loop will continue to count
+                Console.WriteLine(i + "\n");
+                //updating counter
+                i++;
+            }
+            //it will stop counting after counter is 10
+            while (i <= 10);
+            Console.Read();
+        }
+    }
+}
