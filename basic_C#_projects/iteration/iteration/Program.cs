2491b7050cdbd352b07f4a56984d0858979ea362
Maher Dakdouk <mdakdouk2015@outlook.com>
2022-08-26 02:40:11 -0400

    added more loops to project

--- basic_C#_projects/iteration/iteration/Program.cs
+++ basic_C#_projects/iteration/iteration/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+/*** Part 1 ***/
+
 //creating array of strings
 string[] greetings = { "Hello, ", "Hi, ", "Hey, ", "Howdy, ", "Greetings, " };
 
@@ -19,6 +21,40 @@ for (int j = 0; j < greetings.Length; j++)
 {
     //printing each element
     Console.WriteLine("\n" + greetings[j]);
+}
+
+/*** Part 2 ***/
+
+//creating infinite loop
+//for (int k = 0; k < greetings.Length; k++)
+//{
+//    //adding ! to end of each string
+//    greetings[k] = greetings[k] + "!";
+//    //decrementing counter so loop is infinite
+//    k--;
+//}
+
+for (int k = 0; k < greetings.Length; k++)
+{
+    //adding ! to end of each string
+    greetings[k] = greetings[k] + "!";
+    //removed decrement code to fix infinite loop
+}
+
+/*** Part 3 ***/
+
+//creating loop using < condition
+for (int l = 0; l < greetings.Length; l++)
+{
+    //printing out greetings again, with the added exclamation mark
+    Console.WriteLine("\n" + greetings[l]);
+}
+
+//creating loop using <= condition
+for (int m = 0; m <= 4; m++)
+{
+    //adding another exclamation mark to each string
+    greetings[m] = greetings[m] + "!";
 }
 
 Console.ReadLine();
