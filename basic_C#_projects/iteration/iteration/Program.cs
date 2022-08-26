bb9e8c0102b6bb5aa5ed754acfdfed035b5e55f9
Maher Dakdouk <mdakdouk2015@outlook.com>
2022-08-26 02:13:59 -0400

    created program with 2 simple loops

---
+++ basic_C#_projects/iteration/iteration/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//creating array of strings
+string[] greetings = { "Hello, ", "Hi, ", "Hey, ", "Howdy, ", "Greetings, " };
+
+//asking user for text input and storing it as variable
+Console.WriteLine("Please type your name below.");
+string name = Console.ReadLine();
+
+//creating loop to add text input to each greeting
+for (int i = 0; i < greetings.Length; i++)
+{
+    //concatenating strings
+    greetings[i] = greetings[i] + name;
+}
+
+//creating loop to print each element from array
+for (int j = 0; j < greetings.Length; j++)
+{
+    //printing each element
+    Console.WriteLine("\n" + greetings[j]);
+}
+
+Console.ReadLine();
