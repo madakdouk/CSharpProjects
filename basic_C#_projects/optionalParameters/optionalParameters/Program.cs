c1fc275aaa6a04a8c98a1bb18929dc254d501c38
Maher Dakdouk <mdakdouk2015@outlook.com>
2022-08-29 18:56:27 -0400

    method with optional parameter

---
+++ basic_C#_projects/optionalParameters/optionalParameters/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace optionalParameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //calling method with only 1 argument passed to it
+            Console.WriteLine("1 + 1 = " + optional.opt(1));
+
+            //calling method with 2 arguments passed to it
+            Console.WriteLine("4 + 6 = " + optional.opt(4, 6));
+
+            Console.Read();
+        }
+    }
+}
---
+++ basic_C#_projects/optionalParameters/optionalParameters/optional.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace optionalParameters
+{
+    public class optional
+    {
+        //creatig method that will take 2 parameters, but one is optional
+        public static int opt(int n, int m = 1)
+        {
+            //output will be sum of arguments
+            return n + m;
+        }
+    }
+}
