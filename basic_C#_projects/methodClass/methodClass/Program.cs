a2161f85378679d6f699a268cd775e2c8acdc3fc
Maher Dakdouk <mdakdouk2015@outlook.com>
2022-08-30 18:42:53 -0400

    simple console program with class and method taking 2 int parameters

---
+++ basic_C#_projects/methodClass/methodClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodClass
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //instantiating the class
+            methodClass methods = new methodClass();
+            //calling the method and passing in 2 numbers
+            methods.method(3, 9);
+            //calling the method and specifying paramters by name
+            methods.method(x: 7, y: 33);
+            Console.Read();
+        }
+    }
+}
---
+++ basic_C#_projects/methodClass/methodClass/methodClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodClass
+{
+    public class methodClass
+    {
+        //creating void method that takes 2 integers as parameters
+        public void method(int x, int y)
+        {
+            //math operation to be done on first int
+            int operation = x + 5;
+            //displaying second int to screen
+            Console.WriteLine(y);
+        }
+    }
+}
