b84dea821097b1e6d19ea121d38db7d5b40f6977
Maher Dakdouk <mdakdouk2015@outlook.com>
2022-08-31 16:25:59 -0400

    added new classes

---
+++ twentyoneGame/twentyoneGame/game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twentyoneGame
+{
+    public class game
+    {
+        //setting the properties of game class
+        public List<string> players { get; set; }
+        public string name { get; set; }
+        public string dealer { get; set; }
+
+        //method that does not return a value, but prints all players on the screen
+        public void listPlayers()
+        {
+            foreach (string player in players)
+            {
+                Console.WriteLine(player);
+            }
+        }
+    }
+}
---
+++ twentyoneGame/twentyoneGame/twentyOne.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twentyoneGame
+{
+    public class twentyOne : game
+    {
+        public void play()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
