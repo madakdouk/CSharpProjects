7e0576c0e1a58c2cd6b2de1e6280df357b035e51
Maher Dakdouk <mdakdouk2015@outlook.com>
2022-09-01 06:49:14 -0400

    used overloaded operators

---
+++ basic_C#_projects/operators/operators/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operators
+{
+    public class Employee
+    {
+        //creating properties for employee class
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //overload == operator, takes 2 employee objects and returns bool
+        public static bool operator== (Employee emp1, Employee emp2)
+        {
+            //checking if ids are equal
+            bool equalID = emp1.Id == emp2.Id;
+            //print message based on result
+            if (equalID) { Console.WriteLine("The employees have the same ID number."); }
+            else { Console.WriteLine("The employees have unique ID numbers."); }
+            //return bool value: true if equal, false if not
+            return equalID;
+        }
+
+        //overload != operator, takes 2 employeee objects and reutrns bool
+        public static bool operator!= (Employee emp1, Employee emp2)
+        {
+            //check if ids not equal
+            bool notEqualID = emp1.Id != emp2.Id;
+            //print message based on result
+            if (notEqualID) { Console.WriteLine("The employees have unique ID numbers."); }
+            else { Console.WriteLine("The employees have the same ID number."); }
+            //return bool value: true if not equal, false if equal
+            return notEqualID;
+        }
+
+    }
+}
---
+++ basic_C#_projects/operators/operators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operators
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //instantiating 2 new employee objects and setting id properties
+            Employee employee1 = new Employee() { Id = 1 };
+            Employee employee2 = new Employee() { Id = 2 };
+
+            //using overload operator methods
+            bool equalID = employee1 == employee2;
+            bool notEqualID = employee1 != employee2;
+
+            Console.Read();
+        }
+    }
+}
+
