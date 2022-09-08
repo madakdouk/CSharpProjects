efc6fb2e656a3b6996c879a32410034c2923d319
Maher Dakdouk <mdakdouk2015@outlook.com>
2022-09-08 17:07:07 -0400

    simple console app using date time

---
+++ basic_C#_projects/dateTime/dateTime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dateTime
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The current date and time is: "+ DateTime.Now); //displaying current date and time to console
+            Console.WriteLine("Please enter a number."); //asking for user input
+            int hours = Convert.ToInt32(Console.ReadLine()); //storing as var
+            Console.WriteLine("In " + hours + " hours, the date and time will be: " + DateTime.Now.AddHours(hours));
+            Console.Read();
+        }
+    }
+}
--- twentyoneGame/twentyoneGame/dealer.cs
+++ twentyoneGame/twentyoneGame/dealer.cs
@@ -17,6 +17,11 @@ namespace twentyoneGame
             Hand.Add(Deck.Cards.First());
             string card = string.Format(Deck.Cards.First().ToString() + "\n");
             Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\MDakd\Downloads\logs\logtest2.txt", true))
+            {
+                file.WriteLine(DateTime.Now);
+                file.WriteLine(card);
+            }
             Deck.Cards.RemoveAt(0);
         }
     }
