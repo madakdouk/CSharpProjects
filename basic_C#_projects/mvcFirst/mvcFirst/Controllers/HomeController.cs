25d1a2c4eeefa825997b72b724d793ad6cd47d58
Maher Dakdouk <mdakdouk2015@outlook.com>
2022-09-13 16:32:51 -0500

    mvc tutorial

---
+++ basic_C#_projects/mvcFirst/mvcFirst/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvcFirst.Models;
+
+namespace mvcFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            user user = new user();
+            user.ID = 1;
+            user.FirstName = "Maher";
+            user.LastName = "Dakdouk";
+            user.Age = 25;
+            return View(user);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ basic_C#_projects/mvcFirst/mvcFirst/Models/user.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using mvcFirst.Controllers;
+
+namespace mvcFirst.Models
+{
+    public class user
+    {
+        public int ID { get; set; }
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
