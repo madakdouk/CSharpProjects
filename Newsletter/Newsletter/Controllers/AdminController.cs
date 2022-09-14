19e747c15abcd36885f097a165648b5eb3101938
Maher Dakdouk <mdakdouk2015@outlook.com>
2022-09-13 23:50:01 -0500

    adding unsubscribe method

---
+++ Newsletter/Newsletter/Controllers/AdminController.cs
@@ -0,0 +1,43 @@
+﻿using Newsletter.Models;
+using Newsletter.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Newsletter.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (NewsletterEntities db = new NewsletterEntities())
+            {
+                var signups = db.SignUps;
+                var signupVms = new List<SignupVM>();
+                foreach (var signup in signups)
+                {
+                    var signupVm = new SignupVM();
+                    signupVm.Id = signup.Id;
+                    signupVm.FirstName = signup.FirstName;
+                    signupVm.LastName = signup.LastName;
+                    signupVm.EmailAddress = signup.EmailAddress;
+                    signupVms.Add(signupVm);
+                }
+                return View(signupVms);
+            }
+        }
+    }
+    public ActionResult Unsubscribe(int Id)
+    {
+        using (NewsletterEntities db = new NewsletterEntities())
+        {
+            var signup = db.SignUps.Find(Id);
+            signup.Removed = DateTime.Now;
+            db.SaveChanges();
+        }
+        return RedirectToAction("Index", "Admin");
+    }
+}
--- Newsletter/Newsletter/Controllers/HomeController.cs
+++ Newsletter/Newsletter/Controllers/HomeController.cs
@@ -12,8 +12,6 @@ namespace Newsletter.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly string connectionString = @"Data Source=MIMOSLAPTOP\SQLEXPRESS;Initial Catalog=Newsletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-        
         public ActionResult Index()
         {
             return View();
@@ -28,63 +26,18 @@ namespace Newsletter.Controllers
             }
             else
             { 
-                string queryString = @"Insert Into SignUps (FirstName, LastName, EmailAddress) Values
-                                        (@FirstName, @LastName, @EmailAddress)";
-
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                using(NewsletterEntities db = new NewsletterEntities())
                 {
-                    SqlCommand command = new SqlCommand(queryString, connection);
-                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
-                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
-                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
-
-                    command.Parameters["@FirstName"].Value = firstName;
-                    command.Parameters["@LastName"].Value = lastName;
-                    command.Parameters["@EmailAddress"].Value = emailAddress;
-
-                    connection.Open();
-                    command.ExecuteNonQuery();
-                    connection.Close();
+                    var signup = new SignUp();
+                    signup.FirstName = firstName;
+                    signup.LastName = lastName;
+                    signup.EmailAddress = emailAddress;
+                    db.SignUps.Add(signup);
+                    db.SaveChanges();
                 }
                 return View("Success");
             }
         }
 
-        public ActionResult Admin()
-        {
-            string queryString = @"Select Id, FirstName, LastName, EmailAddress, SocialSecurityNumber from SignUps";
-            List<NewsletterSignUp> signups = new List<NewsletterSignUp>();
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                SqlCommand command = new SqlCommand(queryString, connection);
-
-                connection.Open();
-
-                SqlDataReader reader = command.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    var signup = new NewsletterSignUp();
-                    signup.Id = Convert.ToInt32(reader["Id"]);
-                    signup.FirstName = reader["FirstName"].ToString().Trim();
-                    signup.LastName = reader["LastName"].ToString().Trim();
-                    signup.EmailAddress = reader["EmailAddress"].ToString().Trim();
-                    signup.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString().Trim();
-                    signups.Add(signup);
-                }
-            }
-
-            var signupVms = new List<SignupVM>();
-            foreach (var signup in signups)
-            {
-                var signupVm = new SignupVM();
-                signupVm.FirstName = signup.FirstName;
-                signupVm.LastName = signup.LastName;
-                signupVm.EmailAddress = signup.EmailAddress;
-                signupVms.Add(signupVm);
-            }
-            return View(signupVms);
-        }
     }
 }
