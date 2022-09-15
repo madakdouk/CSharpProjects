7410c96666af1c7d26fad876ea1000862c860984
Maher Dakdouk <mdakdouk2015@outlook.com>
2022-09-15 01:30:12 -0500

    created very simple asp.net core web app

---
+++ basic_C#_projects/CoreWebApp/CoreWebApp/Controllers/ProductsController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebApp.Controllers
+{
+    public class ProductsController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
