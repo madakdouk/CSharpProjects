aa3b418452bf4d8c866468e0f9fb9aca0be70ead
Maher Dakdouk <mdakdouk2015@outlook.com>
2022-09-15 00:56:21 -0500

    fixed small error in file path

--- basic_C#_projects/netCoreTutorial/netCoreTutorial/Program.cs
+++ basic_C#_projects/netCoreTutorial/netCoreTutorial/Program.cs
@@ -4,13 +4,13 @@ string name = Console.ReadLine();
 string date = DateTime.Today.ToShortDateString();
 string msg = $"\nWelcome back, {name}. Today is {date}.";
 Console.WriteLine(msg);
-string filePath = @"C:\Users\MDakd\OneDrive\Documents\GitHub\CSharpProjects\basic_C#_projects\netCoreTutorial\netCoreTutorial";
+string filePath = @"C:\Users\MDakd\OneDrive\Documents\GitHub\CSharpProjects\basic_C#_projects\netCoreTutorial\netCoreTutorial\scores.txt";
 string[] lines = System.IO.File.ReadAllLines(filePath);
 double total = 0.0;
 Console.WriteLine("\nStudent Scores: \n");
 foreach (string line in lines)
 {
-    Console.WriteLine("/n" + line);
+    Console.WriteLine(line);
     double score = Convert.ToDouble(line);
     total += score;
 }
