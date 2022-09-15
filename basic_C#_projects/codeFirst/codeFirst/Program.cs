26a0365d9fe6c5311a495e558f14fcb894087fe8
Maher Dakdouk <mdakdouk2015@outlook.com>
2022-09-15 17:03:00 -0500

    program that creates a student and saves to database

--- basic_C#_projects/codeFirst/codeFirst/Program.cs
+++ basic_C#_projects/codeFirst/codeFirst/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace codeFirst
+﻿using System.Data.Entity;
+
+namespace codeFirst
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Gray Town Middle School registration.\n");
+            using (var db = new StudentContext()) //creating new db
+            {
+                //getting student info
+                Console.WriteLine("Please enter the student's first name:");
+                var nameFirst = Console.ReadLine();
+                Console.WriteLine("Please enter the student's last name:");
+                var nameLast = Console.ReadLine();
+                Console.WriteLine("Please enter the student's date of birth in the format MM/DD/YYYY:");
+                DateTime birthdate = Convert.ToDateTime(Console.ReadLine());
+
+                var student = new Student() //creating new student object
+                {
+                    FirstName = nameFirst,
+                    LastName = nameLast,
+                    DOB = birthdate.Date
+                };
+                db.Students.Add(student);
+                db.SaveChanges(); //adding and saving db
+
+                Console.Read();
+            }
         }
     }
 }
---
+++ basic_C#_projects/codeFirst/codeFirst/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace codeFirst
+{
+    public class Student //student class with simple properties
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public double GPA { get; set; }
+
+        public Student() //constructor class
+        {
+            GPA = 0.0; //when new student is created, GPA defaults to 0.0
+        }
+    }
+
+    public class StudentContext : DbContext //for an easy way to read, save data to db
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
